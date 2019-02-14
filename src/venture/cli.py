e85708368113c18ef158eac41f880a15c95fae8a
Greg Trahair <greg@mwb.io>
2019-02-14 15:50:12 +0100

    Added parser options

--- src/venture/cli.py
+++ src/venture/cli.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
-
+import argparse
 
 def main():
-    print('Hello world!')
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+
+    # Add parser for the "create" command
+    create_parser = subparsers.add_parser('create', help="Create a new project")
+    create_parser.add_argument('giturl', help='url of the git repo')
+    # Add parsers for the "workon" command
+    workon_parser = subparsers.add_parser('workon', help="Activate a project")
+
+    args = parser.parse_args()
+    print(args)
